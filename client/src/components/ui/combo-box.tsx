4c2ba767918efced7cc7ecc34ee210a1f89ac31f
Aaron Joseph C. Baquing <aaronbaquing1@gmail.com>
2024-08-04 17:25:22 +0800

    Integrate combo-box with zod and react-hook-form

--- client/src/components/ui/combo-box.tsx
+++ client/src/components/ui/combo-box.tsx
@@ -1,9 +1,7 @@
 "use client";
 
 import React, { useState, useRef, useEffect } from "react";
-import { CaretSortIcon, CheckIcon } from "@radix-ui/react-icons";
-
-import { cn } from "@/lib/utils";
+import { CaretSortIcon } from "@radix-ui/react-icons";
 import { Button } from "@/components/ui/button";
 import {
   Command,
@@ -18,17 +16,7 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-
-const choices = [
-  {
-    value: "ACADEMIC",
-    label: "Academic",
-  },
-  {
-    value: "NON-ACADEMIC",
-    label: "Non-Academic",
-  },
-];
+import { X } from "lucide-react";
 
 interface Choice {
   value: string;
@@ -39,121 +27,127 @@ interface ComboBoxProps {
   choices: Choice[];
   className?: string;
   multiple?: boolean;
+  value: string | string[];
+  onChange: (value: string | string[]) => void;
 }
 
-const ComboBox = ({ className, choices, multiple = false }: ComboBoxProps) => {
-  const [open, setOpen] = useState(false);
-  const [selectedValues, setSelectedValues] = useState<string[]>([]);
+const ComboBox = React.forwardRef<HTMLButtonElement, ComboBoxProps>(
+  ({ className, choices, multiple = false, value, onChange }, ref) => {
+    const [open, setOpen] = useState(false);
+    const [buttonWidth, setButtonWidth] = useState(0);
+    const buttonRef = useRef<HTMLButtonElement>(null);
 
-  // For the PopoverContent to be the same width as the button
-  const [buttonWidth, setButtonWidth] = useState(0);
-  const buttonRef = useRef<HTMLButtonElement>(null);
+    useEffect(() => {
+      const updateButtonWidth = () => {
+        if (buttonRef.current) {
+          setButtonWidth(buttonRef.current.offsetWidth);
+        }
+      };
 
-  // Prod?
-  // Use this later
-  // useEffect(() => {
-  //   if (buttonRef.current) {
-  //     setButtonWidth(buttonRef.current.offsetWidth);
-  //   }
-  // }, []);
+      updateButtonWidth();
 
-  // Dev
-  // Dynamically adjusts its width whenever the button's width changes
-  useEffect(() => {
-    const updateButtonWidth = () => {
+      const resizeObserver = new ResizeObserver(updateButtonWidth);
       if (buttonRef.current) {
-        setButtonWidth(buttonRef.current.offsetWidth);
+        resizeObserver.observe(buttonRef.current);
+      }
+
+      return () => {
+        const currentButtonRef = buttonRef.current;
+        if (currentButtonRef) {
+          resizeObserver.unobserve(currentButtonRef);
+        }
+      };
+    }, []);
+
+    const handleSelect = (selectedValue: string) => {
+      if (multiple) {
+        const newValue = Array.isArray(value) ? value : [];
+        const updatedValue = newValue.includes(selectedValue)
+          ? newValue.filter((v) => v !== selectedValue)
+          : [...newValue, selectedValue];
+        onChange(updatedValue);
+      } else {
+        onChange(selectedValue);
+        setOpen(false);
       }
     };
 
-    updateButtonWidth();
-
-    const resizeObserver = new ResizeObserver(updateButtonWidth);
-    if (buttonRef.current) {
-      resizeObserver.observe(buttonRef.current);
-    }
-
-    return () => {
-      const currentButtonRef = buttonRef.current;
-      if (currentButtonRef) {
-        resizeObserver.unobserve(currentButtonRef);
-      }
+    const handleClear = () => {
+      onChange(multiple ? [] : "");
     };
-  }, []);
 
-  const handleSelect = (value: string) => {
-    if (multiple) {
-      setSelectedValues((prevSelectedValues) =>
-        prevSelectedValues.includes(value)
-          ? prevSelectedValues.filter((v) => v !== value)
-          : [...prevSelectedValues, value]
-      );
-    } else {
-      setSelectedValues([value]);
-      setOpen(false);
-    }
-  };
+    const getDisplayValue = () => {
+      if (multiple && Array.isArray(value)) {
+        return value
+          .map((v) => choices.find((choice) => choice.value === v)?.label)
+          .join(", ");
+      } else if (!multiple && typeof value === "string") {
+        return (
+          choices.find((choice) => choice.value === value)?.label || "Select..."
+        );
+      }
+      return "Select...";
+    };
 
-  return (
-    <div className="w-full flex">
-      <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
-          <Button
-            ref={buttonRef}
-            variant="outline"
-            role="combobox"
-            aria-expanded={open}
-            className={`w-full justify-between ${className}`}
+    return (
+      <div className="w-full flex">
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger asChild>
+            <Button
+              ref={buttonRef}
+              variant="outline"
+              role="combobox"
+              aria-expanded={open}
+              className={`w-full justify-between ${className}`}
+            >
+              {getDisplayValue()}
+              {value && (
+                <Button variant="ghost" onClick={handleClear} className="">
+                  <X />
+                </Button>
+              )}
+              <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent
+            className={`w-full ${className}`}
+            align="end"
+            style={{ width: buttonWidth }}
           >
-            {selectedValues.length > 0
-              ? selectedValues
-                  .map(
-                    (value) =>
-                      choices.find((choice) => choice.value === value)?.label
-                  )
-                  .join(", ")
-              : "Select..."}
-            <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent
-          className={`w-full ${className}`}
-          align="end"
-          style={{ width: buttonWidth }}
-        >
-          <Command>
-            <CommandInput
-              // type="text"
-              placeholder="Search..."
-              className=" h-9 w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-400"
-            />
-            <CommandList>
-              <CommandEmpty>Not found.</CommandEmpty>
-              <CommandGroup className="py-1">
-                {choices.map((choice) => (
-                  <CommandItem
-                    key={choice.value}
-                    className="flex items-center w-full justify-between px-3 py-2 text-left focus:outline-none"
-                    onSelect={() => handleSelect(choice.value)}
-                  >
-                    {multiple && (
-                      <input
-                        type="checkbox"
-                        checked={selectedValues.includes(choice.value)}
-                        onChange={() => handleSelect(choice.value)}
-                        className="mr-2"
-                      />
-                    )}
-                    {choice.label}
-                  </CommandItem>
-                ))}
-              </CommandGroup>
-            </CommandList>
-          </Command>
-        </PopoverContent>
-      </Popover>
-    </div>
-  );
-};
+            <Command>
+              <CommandInput placeholder="Search..." />
+              <CommandList>
+                <CommandEmpty>Not found.</CommandEmpty>
+                <CommandGroup className="py-1">
+                  {choices.map((choice) => (
+                    <CommandItem
+                      key={choice.value}
+                      className="flex items-center w-full justify-between px-3 py-2 text-left focus:outline-none"
+                      onSelect={() => handleSelect(choice.value)}
+                    >
+                      {multiple && (
+                        <input
+                          type="checkbox"
+                          checked={
+                            Array.isArray(value) && value.includes(choice.value)
+                          }
+                          onChange={() => {}}
+                          className="mr-2"
+                        />
+                      )}
+                      {choice.label}
+                    </CommandItem>
+                  ))}
+                </CommandGroup>
+              </CommandList>
+            </Command>
+          </PopoverContent>
+        </Popover>
+      </div>
+    );
+  }
+);
+
+ComboBox.displayName = "ComboBox";
 
 export { ComboBox };
