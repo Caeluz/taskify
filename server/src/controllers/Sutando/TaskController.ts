fdc7b14cb9e5253b66967acfab79199fde35c9fa
Aaron Joseph C. Baquing <aaronbaquing1@gmail.com>
2024-08-13 15:00:40 +0800

    Update TaskController to have show, and delete

--- server/src/controllers/Sutando/TaskController.ts
+++ server/src/controllers/Sutando/TaskController.ts
@@ -20,7 +20,7 @@ export const getProjectTasks = async (req: Request, res: Response) => {
 };
 
 export const createProjectTask = async (req: Request, res: Response) => {
-  const projectId = parseInt(req.params.projectId, 10);
+  const { projectId } = req.params;
   const { name, description, priority, status, startDate, dueDate } = req.body;
   try {
     const project = await Project.query().find(projectId);
@@ -46,5 +46,72 @@ export const createProjectTask = async (req: Request, res: Response) => {
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "error" });
+  }
+};
+
+export const getProjectTask = async (req: Request, res: Response) => {
+  const { projectId, taskId } = req.params;
+  try {
+    const projectTask = await Task.query()
+      .where("project_id", projectId)
+      .find(taskId);
+
+    return res
+      .status(200)
+      .json({ message: "Successfully retrieved task", data: projectTask });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const updateProjectTask = async (req: Request, res: Response) => {
+  const { projectId, taskId } = req.params;
+  const { name, description, priority, status, startDate, dueDate } = req.body;
+
+  try {
+    const projectTask = await Task.query()
+      .where("project_id", projectId)
+      .where("id", taskId)
+      .first();
+
+    if (!projectTask) {
+      return res.status(404).json({ message: "Task or project not found" });
+    }
+
+    projectTask.update({
+      name,
+      description,
+      priority,
+      status,
+      start_date: startDate,
+      due_date: dueDate,
+    });
+
+    return res
+      .status(200)
+      .json({ message: "Successfully updated", data: projectTask });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const deleteProjectTask = async (req: Request, res: Response) => {
+  const { projectId, taskId } = req.params;
+
+  try {
+    const projectTask = await Task.query()
+      .where("project_id", projectId)
+      .where("id", taskId)
+      .firstOrFail();
+
+    await projectTask.delete();
+
+    return res
+      .status(200)
+      .json({ message: "Successfully deleted project's task" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
   }
 };
