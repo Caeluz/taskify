943fb9a788dd91e9cb7998b355080c4d6db41f30
Aaron Joseph C. Baquing <aaronbaquing1@gmail.com>
2024-08-13 15:04:25 +0800

    Update schema of user and project

--- server/src/schemas/ProjectTaskSchema.ts
+++ server/src/schemas/ProjectTaskSchema.ts
@@ -2,21 +2,30 @@ import { z } from "zod";
 
 const dateStringToDate = z.string().transform((str) => new Date(str));
 
-export const createProjectTaskSchema = z.object({
-  // projectId
-  name: z.string(),
-  description: z.string().optional(),
-  priority: z.enum(["low", "medium", "high"]),
-  status: z.string(),
-  startDate: dateStringToDate,
-  dueDate: dateStringToDate,
-});
+export const createProjectTaskSchema = {
+  body: z.object({
+    name: z.string(),
+    description: z.string().optional(),
+    priority: z.enum(["low", "medium", "high"]),
+    status: z.string(),
+    startDate: dateStringToDate,
+    dueDate: dateStringToDate,
+  }),
+  params: z.object({
+    projectId: z.string().regex(/^\d+$/).transform(Number),
+  }),
+};
 
-export const updateProjectTaskSchema = z.object({
-  name: z.string(),
-  description: z.string().optional(),
-  priority: z.enum(["low", "medium", "high"]),
-  status: z.string(),
-  startDate: dateStringToDate,
-  dueDate: dateStringToDate,
-});
+export const updateProjectTaskSchema = {
+  body: z.object({
+    name: z.string(),
+    description: z.string().optional(),
+    priority: z.enum(["low", "medium", "high"]),
+    status: z.string(),
+    startDate: dateStringToDate,
+    dueDate: dateStringToDate,
+  }),
+  params: z.object({
+    projectId: z.string().regex(/^\d+$/).transform(Number),
+  }),
+};
--- server/src/schemas/UserProjectSchema.ts
+++ server/src/schemas/UserProjectSchema.ts
@@ -1,13 +1,23 @@
 import { z } from "zod";
 
-export const createUserProjectSchema = z.object({
-  name: z.string(),
-  description: z.string(),
-  status: z.string(),
-});
+export const createUserProjectSchema = {
+  body: z.object({
+    name: z.string(),
+    description: z.string(),
+    status: z.string(),
+  }),
+  params: z.object({
+    userId: z.string().regex(/^\d+$/).transform(Number),
+  }),
+};
 
-export const updateUserProjectSchema = z.object({
-  name: z.string(),
-  description: z.string(),
-  status: z.string(),
-});
+export const updateUserProjectSchema = {
+  body: z.object({
+    name: z.string(),
+    description: z.string(),
+    status: z.string(),
+  }),
+  // params: z.object({
+  //   projectId: z.string().regex(/^\d+$/).transform(Number),
+  // }),
+};
