844defde511f25a959d8e6360441960717151514
Aaron Joseph C. Baquing <aaronbaquing1@gmail.com>
2024-08-13 15:05:00 +0800

    Add update and delete for task

--- server/src/routes/ProjectRoutes.ts
+++ server/src/routes/ProjectRoutes.ts
@@ -1,19 +1,16 @@
 import { authenticateToken } from "./../middleware/authMiddleware";
 import express from "express";
 import { validateData } from "../middleware/validatorMiddleware";
-import { createUserSchema, updateUserSchema } from "../schemas/UserSchema";
 import { createUserProjectSchema } from "../schemas/UserProjectSchema";
 import * as projectController from "../controllers/Sutando/ProjectController";
 
 const projectRouter = express.Router();
-
+projectRouter.use("/", authenticateToken);
 // Project routes
-projectRouter
-  .get("/projects", authenticateToken, projectController.getProjects)
-  .post("/", validateData(createUserSchema), projectController.createProject);
+projectRouter.get("/projects", projectController.getProjects);
+// .post("/", validateData(createUserSchema), projectController.createProject);
 
 // User project routes
-projectRouter.use("/users", authenticateToken);
 projectRouter
   .get("/users/:userId/projects", projectController.getUserProjects)
   .post(
@@ -21,6 +18,9 @@ projectRouter
     validateData(createUserProjectSchema),
     projectController.createUserProject
   )
-  .delete("/users/:userId/projects/:projectId", projectController.deleteUserProject);
+  .delete(
+    "/users/:userId/projects/:projectId",
+    projectController.deleteUserProject
+  );
 
 export default projectRouter;
--- server/src/routes/TaskRouter.ts
+++ server/src/routes/TaskRouter.ts
@@ -2,7 +2,10 @@ import { authenticateToken } from "./../middleware/authMiddleware";
 import express from "express";
 import * as taskController from "../controllers/Sutando/TaskController";
 import { validateData } from "../middleware/validatorMiddleware";
-import { createProjectTaskSchema } from "../schemas/ProjectTaskSchema";
+import {
+  createProjectTaskSchema,
+  updateProjectTaskSchema,
+} from "../schemas/ProjectTaskSchema";
 
 const taskRouter = express.Router();
 
@@ -13,6 +16,16 @@ taskRouter
     "/projects/:projectId/tasks",
     validateData(createProjectTaskSchema),
     taskController.createProjectTask
+  )
+  .get("/projects/:projectId/tasks/:taskId", taskController.getProjectTask)
+  .put(
+    "/projects/:projectId/tasks/:taskId",
+    validateData(updateProjectTaskSchema),
+    taskController.updateProjectTask
+  )
+  .delete(
+    "/projects/:projectId/tasks/:taskId",
+    taskController.deleteProjectTask
   );
 
 export default taskRouter;
