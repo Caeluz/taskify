6312108e6d7ecda88d964155c046bd989f108398
Aaron Joseph C. Baquing <aaronbaquing1@gmail.com>
2024-09-11 09:14:53 +0800

    Update date-picker-range to accommodate forms

--- client/src/components/ui/date-picker-range.tsx
+++ client/src/components/ui/date-picker-range.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import { CalendarIcon } from "@radix-ui/react-icons";
-import { addDays, format } from "date-fns";
+import { format } from "date-fns";
 import { DateRange } from "react-day-picker";
 
 import { cn } from "@/lib/utils";
@@ -13,21 +13,48 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { cva, VariantProps } from "class-variance-authority";
 
 export interface DatePickerWithRangeProps
   extends React.HTMLAttributes<HTMLDivElement> {
   dateToday?: boolean;
+  dateValue?: DateRange;
+  onDateSelect?: (range: { from: Date; to: Date }) => void;
 }
+
+//
 
 export const DatePickerWithRange: React.FC<DatePickerWithRangeProps> = ({
   className,
   dateToday,
+  dateValue,
+  onDateSelect,
 }) => {
-  const [date, setDate] = React.useState<DateRange | undefined>({
-    from: dateToday ? new Date() : undefined,
-    // to: addDays(new Date(), 20),
-    to: undefined,
-  });
+  // const [date, setDate] = React.useState<DateRange | undefined>(
+  //   value || {
+  //     from: dateToday ? new Date() : undefined,
+  //     to: undefined,
+  //   }
+  // );
+
+  // Set the date to the value passed in
+  const [date, setDate] = React.useState<DateRange | undefined>(
+    dateValue || {
+      from: dateToday ? new Date() : undefined,
+      to: undefined,
+    }
+  );
+
+  const handleDateSelect = (range: DateRange | undefined) => {
+    setDate(range);
+    if (onDateSelect) {
+      onDateSelect({
+        from: range?.from || new Date(),
+        to: range?.to || new Date(),
+      });
+      setDate(range);
+    }
+  };
 
   return (
     <div className={cn("grid gap-2", className)}>
@@ -63,7 +90,7 @@ export const DatePickerWithRange: React.FC<DatePickerWithRangeProps> = ({
             captionLayout="dropdown-buttons"
             defaultMonth={date?.from}
             selected={date}
-            onSelect={setDate}
+            onSelect={handleDateSelect}
             numberOfMonths={2}
             fromYear={1960}
             toYear={new Date().getFullYear() + 5}
